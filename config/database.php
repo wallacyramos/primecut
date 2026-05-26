d3b932c05ed97ae0f8e65a818de02b52225efd3b
Wallacy Ramos Figueiredo <wallacyfigueiredo@guarulhos.sp.gov.br>
2026-05-26 11:13:05 -0300

    debug variaveis railway

--- config/database.php
+++ config/database.php
@@ -1,36 +1,7 @@
 <?php
 
-$databaseUrl = getenv('DATABASE_URL');
-
-if (!$databaseUrl && isset($_ENV['DATABASE_URL'])) {
-    $databaseUrl = $_ENV['DATABASE_URL'];
-}
-
-if (!$databaseUrl && isset($_SERVER['DATABASE_URL'])) {
-    $databaseUrl = $_SERVER['DATABASE_URL'];
-}
-
-if (!$databaseUrl) {
-    die("DATABASE_URL não encontrada.");
-}
-
-$db = parse_url($databaseUrl);
-
-$host = $db['host'] ?? null;
-$port = $db['port'] ?? 5432;
-$user = $db['user'] ?? null;
-$pass = $db['pass'] ?? null;
-$dbname = isset($db['path']) ? ltrim($db['path'], '/') : null;
-
-try {
-    $pdo = new PDO(
-        "pgsql:host=$host;port=$port;dbname=$dbname",
-        $user,
-        $pass
-    );
-
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
-    die("Erro na conexão: " . $e->getMessage());
-}
+echo "<pre>";
+print_r(array_keys($_ENV));
+print_r(array_keys($_SERVER));
+echo "</pre>";
+exit;
