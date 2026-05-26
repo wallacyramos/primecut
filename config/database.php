193dd3adba0683cbd7ae88b6ab0cbfe938d7104c
Wallacy Ramos Figueiredo <wallacyfigueiredo@guarulhos.sp.gov.br>
2026-05-26 11:24:32 -0300

    usando variaveis postgres separadas

--- config/database.php
+++ config/database.php
@@ -1,7 +1,20 @@
 <?php
 
-echo "<pre>";
-print_r(array_keys($_ENV));
-print_r(array_keys($_SERVER));
-echo "</pre>";
-exit;
+$host = getenv('DB_HOST') ?: ($_SERVER['DB_HOST'] ?? null);
+$port = getenv('DB_PORT') ?: ($_SERVER['DB_PORT'] ?? 5432);
+$dbname = getenv('DB_NAME') ?: ($_SERVER['DB_NAME'] ?? null);
+$user = getenv('DB_USER') ?: ($_SERVER['DB_USER'] ?? null);
+$pass = getenv('DB_PASS') ?: ($_SERVER['DB_PASS'] ?? null);
+
+try {
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $user,
+        $pass
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
