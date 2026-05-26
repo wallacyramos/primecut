01c0b63789b7feecf71d85ab567ff11f3803604b
Wallacy Ramos Figueiredo <wallacyfigueiredo@guarulhos.sp.gov.br>
2026-05-26 11:09:13 -0300

    ajustando leitura da DATABASE_URL

--- config/database.php
+++ config/database.php
@@ -2,20 +2,27 @@
 
 $databaseUrl = getenv('DATABASE_URL');
 
+if (!$databaseUrl && isset($_ENV['DATABASE_URL'])) {
+    $databaseUrl = $_ENV['DATABASE_URL'];
+}
+
+if (!$databaseUrl && isset($_SERVER['DATABASE_URL'])) {
+    $databaseUrl = $_SERVER['DATABASE_URL'];
+}
+
 if (!$databaseUrl) {
     die("DATABASE_URL não encontrada.");
 }
 
 $db = parse_url($databaseUrl);
 
-$host = $db['host'];
-$port = $db['port'];
-$user = $db['user'];
-$pass = $db['pass'];
-$dbname = ltrim($db['path'], '/');
+$host = $db['host'] ?? null;
+$port = $db['port'] ?? 5432;
+$user = $db['user'] ?? null;
+$pass = $db['pass'] ?? null;
+$dbname = isset($db['path']) ? ltrim($db['path'], '/') : null;
 
 try {
-
     $pdo = new PDO(
         "pgsql:host=$host;port=$port;dbname=$dbname",
         $user,
@@ -25,6 +32,5 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
-
     die("Erro na conexão: " . $e->getMessage());
 }
