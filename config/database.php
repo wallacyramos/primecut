d747eafdbcb9e0b87f3addf7d42486a5968812b2
Wallacy Ramos Figueiredo <wallacyfigueiredo@guarulhos.sp.gov.br>
2026-05-26 11:43:17 -0300

    troca postgres por sqlite

--- config/database.php
+++ config/database.php
@@ -1,18 +1,9 @@
 <?php
 
-$host = getenv('DB_HOST') ?: ($_SERVER['DB_HOST'] ?? null);
-$port = getenv('DB_PORT') ?: ($_SERVER['DB_PORT'] ?? 5432);
-$dbname = getenv('DB_NAME') ?: ($_SERVER['DB_NAME'] ?? null);
-$user = getenv('DB_USER') ?: ($_SERVER['DB_USER'] ?? null);
-$pass = getenv('DB_PASS') ?: ($_SERVER['DB_PASS'] ?? null);
-
 try {
-    $pdo = new PDO(
-        "pgsql:host=$host;port=$port;dbname=$dbname",
-        $user,
-        $pass
-    );
+    $dbPath = __DIR__ . '/../database.sqlite';
 
+    $pdo = new PDO("sqlite:" . $dbPath);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
